8d233a02fb2db9a55eed6c31083761fc8f92dc3a
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-06-20 14:24:56 +0200

    Déplacement de fichiers + début de la communication API + installation bootstrap

--- generator_pokemon/src/App.js
+++ generator_pokemon/src/App.js
@@ -1,4 +1,35 @@
-function App() {
+import { useEffect, useState } from "react";
+
+const apiUrl = process.env.API_URL;
+
+const App = () => {
+    const [data, setData] = useState(apiUrl);
+
+    useEffect(() => {
+        fetch(`${apiUrl}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erreur dans la réponse');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setData(data);
+            })
+            .catch(error => {
+                console.error('Erreur dans la récupération de data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Mes pokémons aléatoires</h1>
+            <ul>
+                <li>{data}</li>
+            </ul>
+        </div>
+    );
 }
+
 
 export default App;
