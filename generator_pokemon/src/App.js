876c2000f524eb80d7ebeb4cf67229275c9dc6b7
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-06-21 16:14:15 +0200

    nettoyage du fichier app + début de l'appel API dans le Home

--- generator_pokemon/src/App.js
+++ generator_pokemon/src/App.js
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import styles from "./styles/styles.css";
 import Home from "./components/Home";
 import AllTeam from "./components/AllTeam";
 import CreateTeam from "./components/CreateTeam";
@@ -9,28 +7,21 @@ import EditedTeam from "./components/EditedTeam";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const apiUrl = process.env.API_URL;
-
 const App = () => {
-    const [data, setData] = useState(apiUrl);
-
-    // useEffect(() => {
-    //     fetch(`${apiUrl}`)
-    //         .then
-    //         .then
-    //         .catch
-    // }, []);
-
     return (
         <Router>
+            <div className="d-flex flex-column min-vh-100">
                 <Header />
+                <div className="container flex-grow-1">
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/all_team" element={<AllTeam />} />
                         <Route path="/create_team" element={<CreateTeam />} />
                         <Route path="/edited_team" element={<EditedTeam />} />
                     </Routes>
+                </div>
                 <Footer />
+            </div>
         </Router>
     );
 }
--- generator_pokemon/src/components/Home.js
+++ generator_pokemon/src/components/Home.js
@@ -1,14 +1,33 @@
-import styles from "../styles/styles.css"; 
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [pokemons, setPokemon] = useState();
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=-1"
+
+    useEffect(() => {
+        const pokemonApi = async() = {
+            try {
+                const response = await fetch(apiUrl)
+            } catch(error) {
+                console.error("Erreur pendant la récupération:", error);
+            }
+        }
+
+    }, [apiUrl])
+
     return (
-        <>
+        <div>
             <h1 className="title">Liste des pokémons</h1>
-            <ul>
-                <li></li>
-            </ul>
-        </>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
 export default Home;
+
+// faire une boucle pour chaque pokémon 
