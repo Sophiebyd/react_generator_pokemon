d16cdc0ea8106c0c8ba15397f95bf72c5f845a5f
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-06-21 14:01:37 +0200

    ajout des redirections

--- generator_pokemon/src/App.js
+++ generator_pokemon/src/App.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import styles from "./styles/styles.css";
-// import Home from "./components/Home";
-// import AllTeam from "./components/AllTeam";
-// import CreateTeam from "./components/CreateTeam";
-// import EditedTeam from "./components/EditedTeam";
+import Home from "./components/Home";
+import AllTeam from "./components/AllTeam";
+import CreateTeam from "./components/CreateTeam";
+import EditedTeam from "./components/EditedTeam";
 
-// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const apiUrl = process.env.API_URL;
 
@@ -22,23 +22,21 @@ const App = () => {
     // }, []);
 
     return (
-        <>
-            <Header />
-            <h1 className="title">Liste des pokémons</h1>
-            <ul>
-                <li></li>
-            </ul>
-            {/* <Router>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/AllTeam" component={AllTeam} />
-                    <Route path="/CreateTeam" component={CreateTeam} />
-                    <Route path="/EditedTeam" component={EditedTeam} />
-            </Router> */}
-    <Footer />
-
-        </>
+        <Router>
+            <div className="d-flex flex-column min-vh-100">
+                <Header />
+                <div className="container flex-grow-1">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/all_team" element={<AllTeam />} />
+                        <Route path="/create_team" element={<CreateTeam />} />
+                        <Route path="/edited_team" element={<EditedTeam />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
     );
-
 }
 
 
--- generator_pokemon/src/components/Header.js
+++ generator_pokemon/src/components/Header.js
@@ -1,35 +1,35 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Générateur de Pokémon</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Accueil</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Toutes les équipe de pokémon aléatoire crées</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Générer une équipe de pokémon aléatoire</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Modifier une équipe existante</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">Générateur de Pokémon</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/">Accueil</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/all_team">Toutes les équipes de Pokémon aléatoires créées</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/create_team">Générer une équipe de Pokémon aléatoire</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/edited_team">Modifier une équipe existante</Link>
+                    </li>
+                </ul>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
-        </nav>
+        </div>
+    </nav>
     );
 }
 
