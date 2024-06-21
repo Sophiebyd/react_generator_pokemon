fa72cdbc725527b157328cccf30d2425cf2337b8
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-06-21 14:02:20 +0200

    modification de la page d'accueil

--- generator_pokemon/src/App.js
+++ generator_pokemon/src/App.js
@@ -23,18 +23,14 @@ const App = () => {
 
     return (
         <Router>
-            <div className="d-flex flex-column min-vh-100">
                 <Header />
-                <div className="container flex-grow-1">
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/all_team" element={<AllTeam />} />
                         <Route path="/create_team" element={<CreateTeam />} />
                         <Route path="/edited_team" element={<EditedTeam />} />
                     </Routes>
-                </div>
                 <Footer />
-            </div>
         </Router>
     );
 }
