9809c08f7299c804ffb0d1c95d3bb5f333fc82c1
Sophiebyd <80777640+Sophiebyd@users.noreply.github.com>
2024-06-24 12:07:27 +0200

    correction fetch get API pokéAPI + modification cards + ajout commentaires. Problèmes avec promise en cours de rectification

--- generator_pokemon/src/components/Home.js
+++ generator_pokemon/src/components/Home.js
@@ -1,32 +1,48 @@
 import { useEffect, useState } from "react";
 
 const Home = () => {
-    const [pokemons, setPokemon] = useState();
+    const [pokemons, setPokemons] = useState([]); // mets à jour le composant avec de nouvelles données
     const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=-1"
 
+    // se connecter à un système extérieur
     useEffect(() => {
-        const pokemonApi = async() = {
+        const pokemonApi = async () => {
             try {
-                const response = await fetch(apiUrl)
-            } catch(error) {
+                const response = await fetch(apiUrl); // faire un get à l'API
+                if (!response.ok) {
+                    throw new Error("La réponse n'est pas OK"); // check si le fetch fonctionne
+                }
+                const data = await response.json(); // transforme la réponse en JSON
+                console.log(data);
+                const pokemonResult = data.results; // liste des résultats
+                setPokemons(pokemonResult); // mets à jour l'état
+            } catch (error) {
                 console.error("Erreur pendant la récupération:", error);
             }
-        }
-
-    }, [apiUrl])
-
+        };
+        pokemonApi()
+    }, []);
+    
     return (
-        <div>
+        <div className="container">
             <h1 className="title">Liste des pokémons</h1>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
+            <div className="row">
+                {pokemons.map((pokemon, index) => (
+                    <div key={index} className="col-md-4 mb-4">
+                        <div className="card h-100">
+                            <img src="..." className="card-img-top" alt='...' />
+                            <div className="card-body">
+                                <h5 className="card-title">Nom du pokémon</h5>
+                                <p className="card-text"></p>
+                                <p className="card-text"></p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
-}
+};
 
 export default Home;
 
